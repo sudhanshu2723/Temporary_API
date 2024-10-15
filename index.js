30ca121956b10a6c71e642c82f9b35d87a4f929a
Sudhanshu2723 <141790351+sudhanshu2723@users.noreply.github.com>
2024-10-15 18:34:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express=require("express")
-
-const app=express();
-
-app.get("/",(req,res)=>{
-    return res.json({
-        msg:"you are in login page"
-    })
-})
-
-app.listen(3000);
+ msg:"you are in login page"
